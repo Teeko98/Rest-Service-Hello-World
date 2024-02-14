4e1311e9435b4b2c2dc162be3df96220b4b048a7
Alberto Sosa <aesosa@proton.me>
2024-02-14 12:11:26 -0500

    Initial Commit

---
+++ src/main/java/com/york/alberto/restservicehelloworld/RestServiceHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.york.alberto.restservicehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/york/alberto/restservicehelloworld/RestServiceHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.york.alberto.restservicehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
